21cb784f93ce60b870c3b6693f856da8a834e156
Andreas Marr <andreasm@artcom.de>
2014-08-07 22:04:22 +0200

    Added some more dole

--- lib/dole.nim
+++ lib/dole.nim
@@ -1,2 +1,18 @@
+import math
+
+proc scaleCubeRootMass(scale: float, mass: float): float =
+  return scale * Math.pow(mass, 1.0/3.0)
+
+
+proc innermostPlanet*(stellar_mass: float): float =
+  return scaleCubeRootMass(0.3, stellar_mass)
+
+proc outermostPlanet*(stellar_mass: float): float =
+  return scaleCubeRootMass(50.0, stellar_mass)
+
+
 proc innerDustLimit*(stellar_mass: float): float =
   return 0.0
+
+proc outerDustLimit*(stellar_mass: float): float =
+  return scaleCubeRootMass(200.0, stellar_mass)
