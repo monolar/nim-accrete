f8aa954596480df22fceb8d6174e2b08cb17acd7
Andreas Marr <andreasm@artcom.de>
2014-08-07 22:04:44 +0200

    Added an accretion disk as type

---
+++ lib/accretion_disk.nim
@@ -0,0 +1,17 @@
+import star, dole
+
+type
+  AccretionDisk* = object
+    star*: Star
+
+method innerBound*(this: AccretionDisk): float =
+    return dole.innermostPlanet(this.star.mass)
+
+method outerBound*(this: AccretionDisk): float =
+    return dole.outermostPlanet(this.star.mass)
+
+method innerDust*(this: AccretionDisk): float =
+    return dole.innerDustLimit(this.star.mass)
+
+method outerDust*(this: AccretionDisk): float =
+    return dole.outerDustLimit(this.star.mass)
